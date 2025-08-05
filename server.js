6872ffa22cbf8cf00c163fc1c819fff7e0dd4ab7
Beliver-247 <mendisdumindu21@gmail.com>
2025-08-05 15:40:53 +0530

    removed users

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
